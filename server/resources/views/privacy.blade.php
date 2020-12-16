57fc91c56f7791071a8758c0603dddc03845ae82
Patrick Hessinger <patrick.hessinger@brainvation.de>
2020-12-16 23:58:50 +0100

    corrections basic html - going to bed now

--- server/resources/views/privacy.blade.php
+++ server/resources/views/privacy.blade.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Hohl den Pokal! - Datenschutz</title>
-        {{ HTML::script('js/app.js'); }}
-        {{ HTML::style('css/app.css'); }}
-    </head>
-    <body>
-        <p>
-            Die Datenschutz-Erklärung ist noch nicht erstellt.
-        </p>
-        <p>
-            Dies hat vor allem damit zu tun, dass die App noch nicht für die Öffentlichkeit freigegeben ist.
-        </p>
-        <p>
-            Gebe keine Echtdaten ein und verwende den Bot erst, wenn er offiziell freigeeben ist.
-        </p>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Hohl den Pokal! - Datenschutz</title>
+    <link rel='stylesheet' href="{{ URL::asset('css/app.css') }}" />
+    <script src="{{ URL::asset('js/app.js') }}"></script>
+</head>
+
+<body>
+    <p>
+        Die Datenschutz-Erklärung ist noch nicht erstellt.
+    </p>
+    <p>
+        Dies hat vor allem damit zu tun, dass die App noch nicht für die Öffentlichkeit freigegeben ist.
+    </p>
+    <p>
+        Gebe keine Echtdaten ein und verwende den Bot erst, wenn er offiziell freigeeben ist.
+    </p>
+</body>
+
 </html>
--- server/resources/views/welcome.blade.php
+++ server/resources/views/welcome.blade.php
@@ -1,23 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <title>Hohl den Pokal!</title>
-        {{ HTML::script('js/app.js'); }}
-        {{ HTML::style('css/app.css'); }}
-    </head>
-    <body>
-        <p>
-            Dieser Bot führt dich durch "Hohl den Pokal!" das interaktive Spiel für die nächste 
-            Party mit deinen Freunden. 
-        </p>
-        <p>
-            Ihr spielt gemeinsam in mehreren Spielen gegeneinander, und der beste hohl sich den Pokal. 
-            Wirst du es diesmal sein?
-        </p>
-        <p>
-            Wir befinden uns noch in der Entwicklung. 
-            Bitte nutze den Bot noch nicht, wenn du nicht zu den Alpha-Testern gehörst.
-        </p>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <title>Hohl den Pokal!</title>
+    <link rel='stylesheet' href="{{ URL::asset('css/app.css') }}" />
+    <script src="{{ URL::asset('js/app.js') }}"></script>
+</head>
+
+<body>
+    <p>
+        Dieser Bot führt dich durch "Hohl den Pokal!" das interaktive Spiel für die nächste
+        Party mit deinen Freunden.
+    </p>
+    <p>
+        Ihr spielt gemeinsam in mehreren Spielen gegeneinander, und der beste hohl sich den Pokal.
+        Wirst du es diesmal sein?
+    </p>
+    <p>
+        Wir befinden uns noch in der Entwicklung.
+        Bitte nutze den Bot noch nicht, wenn du nicht zu den Alpha-Testern gehörst.
+    </p>
+</body>
+
 </html>
