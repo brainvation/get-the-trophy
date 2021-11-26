877deaa6b445cb685c0c08d93b7b7af54c5e0823
Patrick Hessinger <patrick.hessinger@brainvation.de>
2021-11-26 12:21:08 +0100

    Code Quality Fixes

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,5 +1,7 @@
 <?php
 
+// phpcs:disable PSR1.Classes.ClassDeclaration -- Namespace would break Laravel Migration
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
