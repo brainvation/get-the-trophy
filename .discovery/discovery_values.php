f51a3057c8827c8d3b21de7e4d366e17c8c9c12e
Patrick Hessinger <patrick.hessinger@brainvation.de>
2022-06-05 18:07:34 +0200

    Create Automated tests Fixes #44

---
+++ .discovery/discovery_values.php
@@ -0,0 +1,23 @@
+<?php
+return array (
+  'botman/driver-config' => 
+  array (
+    0 => 'stubs/telegram.php',
+    1 => 'stubs/web.php',
+  ),
+  'botman/driver' => 
+  array (
+    0 => 'BotMan\\Drivers\\Telegram\\TelegramDriver',
+    1 => 'BotMan\\Drivers\\Telegram\\TelegramAudioDriver',
+    2 => 'BotMan\\Drivers\\Telegram\\TelegramFileDriver',
+    3 => 'BotMan\\Drivers\\Telegram\\TelegramLocationDriver',
+    4 => 'BotMan\\Drivers\\Telegram\\TelegramContactDriver',
+    5 => 'BotMan\\Drivers\\Telegram\\TelegramPhotoDriver',
+    6 => 'BotMan\\Drivers\\Telegram\\TelegramVideoDriver',
+    7 => 'BotMan\\Drivers\\Web\\WebDriver',
+  ),
+  'botman/commands' => 
+  array (
+    0 => 'BotMan\\Drivers\\Telegram\\Console\\Commands\\TelegramRegisterCommand',
+  ),
+);
---
+++ tests/Feature/InitialSetupTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Feature;
+
+use BotMan\BotMan\BotMan;
+use Tests\TestCase;
+
+class InitialSetupTest extends TestCase
+{
+    protected BotMan $botman;
+
+    public function testHappyFlow()
+    {
+        $this->bot
+            ->receives('Hi')
+            ->assertReply('x');
+    }
+}
